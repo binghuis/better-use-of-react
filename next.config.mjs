6809ee08812f55b24523b402977902a4065cf502
songbinghui <soongbinghui@gmail.com>
2024-02-04 19:09:04 +0800

    feat: import fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ import nextra from "nextra";
 const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  latex: true,
-  flexsearch: {
-    codeblock: false,
-  },
 });
 
 export default withNextra({
