818cfd301f2d6943a7f863c70c87106aa71e1bef
songbinghui <soongbinghui@gmail.com>
2024-02-22 14:21:16 +0800

    feat: ts

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,31 @@
-import nextra from "nextra";
+import nextra from 'nextra'
 
 const withNextra = nextra({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
   latex: true,
-});
+  defaultShowCopyCode: true
+})
 
 export default withNextra({
   experimental: {
-    typedRoutes: true,
+    typedRoutes: true
   },
   i18n: {
-    locales: ["zh-CN"],
-    defaultLocale: "zh-CN",
+    locales: ['zh-CN'],
+    defaultLocale: 'zh-CN'
   },
   redirects: () => [
     {
-      source: "/docs.([a-zA-Z-]+)",
-      destination: "/docs/getting-started",
-      statusCode: 302,
+      source: '/docs.([a-zA-Z-]+)',
+      destination: '/docs/getting-started',
+      statusCode: 302
     },
     {
-      source: "/docs",
-      destination: "/docs/getting-started",
-      statusCode: 302,
-    },
+      source: '/docs',
+      destination: '/docs/getting-started',
+      statusCode: 302
+    }
   ],
-  reactStrictMode: true,
-});
+  reactStrictMode: true
+})
