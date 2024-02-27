f0de2274f10a0c0534178b74a8ec3a7adfc2849b
songbinghui <soongbinghui@gmail.com>
2024-02-27 19:49:03 +0800

    feat: demo counter components

--- demo/web-debug/src/components/Counter.tsx
+++ demo/web-debug/src/components/Counter.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 type Props = { initialCount: number }
 
 const Counter = (props: Props) => {
   const { initialCount } = props
   const [count, setCount] = useState(initialCount)
+  console.log('Counter render')
+
+  useEffect(() => {
+    console.log('Counter loaded')
+    return () => {
+      console.log('Counter unloaded')
+    }
+  }, [])
 
   return (
     <div className="flex space-x-2 items-center border p-4 rounded ">
--- demo/web-debug/src/components/CounterWrapper.tsx
+++ demo/web-debug/src/components/CounterWrapper.tsx
@@ -1,24 +1,39 @@
 import { useState } from 'react'
 import Counter from './Counter'
 
+import { useReducer } from 'react'
+
+const updateReducer = (num: number): number => (num + 1) % 1_000_000
+
+function useUpdate(): () => void {
+  const [, update] = useReducer(updateReducer, 0)
+
+  return update
+}
+
 const CounterWrapper = () => {
   const [count, setCount] = useState(0)
+  const update = useUpdate()
 
   return (
-    <div className="border rounded p-4">
-      <label htmlFor="name">计数器初始值：</label>
-
-      <input
-        type="number"
-        id="name"
-        name="name"
-        className="border mb-4 p-2 rounded"
-        value={count}
-        onChange={e => {
-          setCount(parseInt(e.target.value))
-        }}
-      />
-
+    <div className="border rounded p-4 space-y-4">
+      <div className="flex space-x-2">
+        <div>
+          <label htmlFor="counter">计数器初始值：</label>
+          <input
+            type="number"
+            id="counter"
+            name="counter"
+            className="border p-2 rounded"
+            value={count}
+            onChange={e => {
+              setCount(parseInt(e.target.value))
+            }}
+          />
+        </div>
+        <button onClick={update}>重渲染</button>
+      </div>
+      {/* <Counter initialCount={count} key={count} /> */}
       <Counter initialCount={count} />
     </div>
   )
