44b23d3052c40ddab1c5b42caea0c95235a49891
宋秉徽 <451000936@qq.com>
2024-04-08 13:15:22 +0800

    feat: code box

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,20 +1,60 @@
 import { Sandpack, SandpackThemeProp, SandpackProps, SandpackFile } from '@codesandbox/sandpack-react'
 import { useTheme } from 'nextra-theme-docs'
 import { useEffect, useState, PropsWithChildren } from 'react'
+import { hash } from 'spark-md5'
+function genCodeString(node: string | React.ReactElement | React.ReactElement[]) {
+  let codeString = ''
 
-const CodeBlock = (props: React.PropsWithChildren) => {
+  if (typeof node === 'string') {
+    codeString += node
+  } else if (!Array.isArray(node)) {
+    if (node.props.children) {
+      codeString += node.props.children
+    }
+  } else {
+    node.forEach(child => {
+      if (typeof child === 'string') {
+        codeString += child
+      } else {
+        codeString += genCodeString(child.props.children)
+      }
+    })
+  }
+  return codeString
+}
+
+function genFilesOpts(node: React.ReactElement | React.ReactElement[]) {
+  if (!Array.isArray(node)) {
+    const { 'data-language': lang, children } = node.props
+    const [fileName, language] = lang.split('.')
+    return { [fileName && language ? `${fileName}.tsx` : 'App.tsx']: { code: genCodeString(children.props.children) } }
+  } else {
+    return node.reduce((acc, child) => {
+      const { 'data-language': lang, children } = child.props
+      const [fileName, language] = lang.split('.')
+      acc[fileName && language ? `${fileName}.tsx` : 'App.tsx'] = { code: genCodeString(children.props.children) }
+      return acc
+    }, {})
+  }
+}
+
+const CodeBlock = (props: PropsWithChildren<React.ReactElement | React.ReactElement[]>) => {
   const { theme } = useTheme()
   const [sandpackTheme, setSandpackTheme] = useState<SandpackThemeProp>()
-
-  console.log(props.children)
+  const [files, setFiles] = useState<Record<string, SandpackFile>>({})
 
   useEffect(() => {
+    setFiles(genFilesOpts(props.children))
     setSandpackTheme({ light: 'light', dark: 'dark', system: 'auto' }[theme] ?? 'dark')
-  }, [theme])
+  }, [theme, props.children])
+
+  if (!props.children) return null
 
   return (
     <div className="my-4">
       <Sandpack
+        files={files}
+        key={hash(JSON.stringify(files))}
         theme={sandpackTheme}
         options={{
           showConsole: true,
@@ -24,7 +64,7 @@ const CodeBlock = (props: React.PropsWithChildren) => {
           classes: {},
           autoReload: false
         }}
-        template="react"
+        template="react-ts"
       />
     </div>
   )
